aea923ae223c6ce4da01e1170df702725672b8c8
Vivek <vivejadha@gmail.com>
2022-04-06 10:28:56 +0530

    Aprili 06,2022

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -16,7 +16,7 @@ const NavigationBar = () => {
                         <Nav.Link href="/" className='me-4'><i className="fas fa-home icon"></i>Home</Nav.Link>
                         <Nav.Link href="/fitness" className='me-4'><i className="fas fa-biking icon"></i>Fitness</Nav.Link>
                         <Nav.Link href="/covid-19" className='me-4'><i className="fas fa-virus icon"></i>Covid-19</Nav.Link>
-                        <Nav.Link href="https://www.youtube.com/channel/UCb6ZlyWymyuDdBVbhVtJp7Q" target='_blank' className='me-4'><i className="fab fa-youtube icon"></i>YouTube</Nav.Link>
+                        <Nav.Link href="https://www.youtube.com/channel/UCb6ZlyWymyuDdBVbhVtJp7Q?sub_confirmation=1" target='_blank' className='me-4'><i className="fab fa-youtube icon"></i>YouTube</Nav.Link>
                         <Nav.Link href="https://www.instagram.com/yohohealth/" target='_blank' className='me-4'><i className="fab fa-instagram icon"></i>Instagram</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
